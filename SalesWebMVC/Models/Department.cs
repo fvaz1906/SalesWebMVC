1b6c39423f2c7d3786ffdf0b7ae3417efa3ec655
Felipe Vaz <felipevaz@live.com>
2020-11-16 19:51:11 -0300

    feature: adicionado novas entidades e migrations

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace SalesWebMVC.Models
 {
@@ -8,5 +10,23 @@ namespace SalesWebMVC.Models
         public string Name { get; set; }
 
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller s)
+        {
+            Sellers.Add(s);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
     }
 }
