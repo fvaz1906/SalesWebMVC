02ea760d6f74d400fb27edc7aa83fe299b04c3bf
Felipe Vaz <felipevaz@live.com>
2020-11-16 22:54:19 -0300

    feature: adicionado lista combobox para adicionar departamentos ao vendedor

---
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+using SalesWebMVC.Data;
+
+namespace SalesWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public DepartmentService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
